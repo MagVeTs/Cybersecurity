bdd5091f78838935feeefe6648e0235039e5fd3b
MagVeTs <109907528+MagVeTs@users.noreply.github.com>
2023-09-03 12:44:47 +0300

    Update secretsdump.py

--- tools/secretsdump.py
+++ tools/secretsdump.py
@@ -1,3 +1,6 @@
 secretsdump.py
 ---------------
-
+from: Heath Adams - The Cyber Mentor ; Practical Ethical Hacking - The Complete Course ; https://academy.tcm-sec.com/courses/1152300/lectures/48489922
+syntax:
+$ secretsdump.py <domain>/<username>:'<password>'@<target_ip>
+- dumps many types of secrets (including SAM hashes)
