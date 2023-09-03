563feb1eb04d8a9e63843a8f43e765672e3a44a2
MagVeTs <109907528+MagVeTs@users.noreply.github.com>
2023-09-03 17:47:08 +0300

    Update secretsdump.py

--- tools/secretsdump.py
+++ tools/secretsdump.py
@@ -1,6 +1,9 @@
 secretsdump.py
+
 ---------------
 from: Heath Adams - The Cyber Mentor ; Practical Ethical Hacking - The Complete Course ; https://academy.tcm-sec.com/courses/1152300/lectures/48489922
+- dumps many types of secrets (including SAM hashes)
 syntax:
 $ secretsdump.py <domain>/<username>:'<password>'@<target_ip>
-- dumps many types of secrets (including SAM hashes)
+OR
+$ secretsdump.py <username>@<target_ip> -hashes <NTLMv1_hash>
