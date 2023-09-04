cc713fe4304909c8569f002364d084e369adca98
MagVeTs <109907528+MagVeTs@users.noreply.github.com>
2023-09-04 16:42:51 +0300

    Update GetUserSPNs.py

--- tools/GetUserSPNs.py
+++ tools/GetUserSPNs.py
@@ -1,5 +1,10 @@
 GetUserSPNs.py
 ---------------
-from: Heath Adams - The Cyber Mentor ; Practical Ethical Hacking - The Complete Course ; "Kerberoasting Overview" ; https://academy.tcm-sec.com/courses/1152300/lectures/48493804
-$ python GetUserSPNs.py <DOMAIN/username:password> -dc-ip <ip_of_DC> -request
+from: Heath Adams - The Cyber Mentor ; Practical Ethical Hacking - The Complete Course
+"Kerberoasting Overview" ; https://academy.tcm-sec.com/courses/1152300/lectures/48493804
+"Kerberoasting Walkthrough" ; https://academy.tcm-sec.com/courses/1152300/lectures/48493807
+
+$ sudo GetUserSPNs.py <DOMAIN/username:password> -dc-ip <ip_of_DC> -request
+
 - this will dump the Kerberos hash from the DC which you will then attempt to crack (e.g. using hashcat)
+- SPN stands for ServicePrincipalName ; a service account within the AD
