62a53c84b489a03e453e27d7de559439faebf8b4
MagVeTs <109907528+MagVeTs@users.noreply.github.com>
2023-09-04 13:22:02 +0300

    Update GetUserSPNs.py

--- tools/GetUserSPNs.py
+++ tools/GetUserSPNs.py
@@ -2,3 +2,4 @@ GetUserSPNs.py
 ---------------
 from: Heath Adams - The Cyber Mentor ; Practical Ethical Hacking - The Complete Course ; "Kerberoasting Overview" ; https://academy.tcm-sec.com/courses/1152300/lectures/48493804
 $ python GetUserSPNs.py <DOMAIN/username:password> -dc-ip <ip_of_DC> -request
+- this will dump the hash which will then be cracked (e.g. using hashcat)
