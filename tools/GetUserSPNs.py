b6013f6c7f973b1c90b2360e80ff677966bab7f1
MagVeTs <109907528+MagVeTs@users.noreply.github.com>
2023-09-04 16:48:05 +0300

    Update GetUserSPNs.py

--- tools/GetUserSPNs.py
+++ tools/GetUserSPNs.py
@@ -8,3 +8,5 @@ $ sudo GetUserSPNs.py <DOMAIN/username:password> -dc-ip <ip_of_DC> -request
 
 - this will dump the Kerberos hash from the DC which you will then attempt to crack (e.g. using hashcat)
 - SPN stands for ServicePrincipalName ; a service account (e.g. SQLService) within the AD
+- once Kerberos hash is dumped copy the entire hash (starting from $krb5tgs and going all the way to the end) and paste it into a text doc for cracking
+- the hashcat mode for Kerberos hashes is 13100
