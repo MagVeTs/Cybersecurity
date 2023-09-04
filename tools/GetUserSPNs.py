26bb16c9cb9b5261186ca7839330ed58dbd492ae
MagVeTs <109907528+MagVeTs@users.noreply.github.com>
2023-09-04 13:20:38 +0300

    Update GetUserSPNs.py

--- tools/GetUserSPNs.py
+++ tools/GetUserSPNs.py
@@ -1,4 +1,4 @@
 GetUserSPNs.py
 ---------------
-from: https://academy.tcm-sec.com/courses/1152300/lectures/48493804
+from: Heath Adams - The Cyber Mentor ; Practical Ethical Hacking - The Complete Course ; "Kerberoasting Overview" ; https://academy.tcm-sec.com/courses/1152300/lectures/48493804
 $ python GetUserSPNs.py <DOMAIN/username:password> -dc-ip <ip_of_DC> -request
