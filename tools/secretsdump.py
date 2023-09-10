e709f6518bad045e66a05f6743ae5cd9f2c26725
MagVeTs <109907528+MagVeTs@users.noreply.github.com>
2023-09-10 09:10:35 +0300

    Update secretsdump.py

--- tools/secretsdump.py
+++ tools/secretsdump.py
@@ -7,3 +7,14 @@ syntax:
 $ secretsdump.py <domain>/<username>:'<password>'@<target_ip>
 OR
 $ secretsdump.py <username>@<target_ip> -hashes <NTLMv1_hash>
+---------------
+from: Heath Adams - The Cyber Mentor ; Practical Ethical Hacking - The Complete Course
+"Dumping the NTDS.dit" - https://academy.tcm-sec.com/courses/1152300/lectures/48496935
+
+- the NTDS.dit is a database for storing AD data; including:
+* user info
+* group info
+* security descriptors
+* password hashes
+- it can be dumped using secretsdump.py with the following suntax:
+$ secretsdump.py <domain>/<username>:'<password>'@<target_ip> -just-dc-ntlm
